9fc3744ddbae597703305ac5707e8ff455e53393
Rustam Mustafoev <rustam.mustafoev01@gmail.com>
2021-11-07 12:30:29 +0500

    Updated unit tests

--- books_api/store/tests/test_serializers.py
+++ books_api/store/tests/test_serializers.py
@@ -7,10 +7,17 @@ class BookSerializerTestCase(TestCase):
     def test_ok(self):
         book_1 = Book.objects.create(name='Rework', price=75.00)
         book_2 = Book.objects.create(name='Everything is figureoutable', price=80.00)
-        data = BooksSerializer([book_1, book_2], many=True)
+        data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
-                'name': 'Rework'
-            }
+                'name': 'Rework',
+                'price': '75.00',
+            },
+            {
+                'id': book_2.id,
+                'name': 'Everything is figureoutable',
+                'price': '80.00',
+            },
         ]
+        self.assertEqual(expected_data, data)
